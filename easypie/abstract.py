436a7a44e77d47dd22d4011a6d4dab3c17827000
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-07 15:41:59 +0200

    Added Animation prototype. (This is a testing commit for a bug in Pycharm)

--- easypie/abstract.py
+++ easypie/abstract.py
@@ -1,13 +1,15 @@
 # coding=utf-8
 import pygame
 
-
 class GameElement(pygame.sprite.Sprite):  # TODO Create game element bindings
 
     def __init__(self):
         super(GameElement, self).__init__()
         self.x = 300
         pass
+
+    def move(self):
+        pass #todo fix gameloop animations with yield
 
     def _move(self):
         import math
@@ -16,3 +18,14 @@ class GameElement(pygame.sprite.Sprite):  # TODO Create game element bindings
         direction_radians = math.radians(self.direction)
         self.x += self.speed * math.sin(direction_radians)
         self.y -= self.speed * math.cos(direction_radians)
+
+class Animation():#todo create animation
+
+    def __init__(self):
+        self.key_frames = []
+
+    def append(self,kf):
+        self.key_frames.append(kf)
+
+    def next(self):
+        self.key_frames.pop(0)()
