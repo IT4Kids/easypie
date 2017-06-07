eadd266006e88d19a6ba8de90ad9875b5d071822
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-07 17:34:04 +0200

    Removed error highlight in editor test.

--- gui/editor.py
+++ gui/editor.py
@@ -13,12 +13,4 @@ class Editor(PyCodeEdit):
     def __init__(self):
         super().__init__(server_script=server.__file__, color_scheme='monokai')
         self.modes.remove(PEP8CheckerMode)
-        #self.decorations.append(ErrorHighlight(40, self.textCursor()))#todo ?
 
-
-class ErrorHighlight(TextDecoration):
-    def __init__(self, lineno, cursor_or_bloc_or_doc):  # todo
-        super().__init__(cursor_or_bloc_or_doc, full_width=True, start_line=lineno, end_line=lineno,
-                         tooltip="Error bla bla")
-        brush = QtGui.QBrush(QColor(255, 255, 100))
-        self.set_background(brush)
