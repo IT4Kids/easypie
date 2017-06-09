06f58791863a2e3fc3db9728bd8fab11c78dd104
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-09 10:04:45 +0200

    Updated user-bindings and added user-constants.

--- easypie/__init__.py
+++ easypie/__init__.py
@@ -1,6 +1,20 @@
 # coding=utf-8
 from pygame import *
-from easypie.constants import *
+import PyQt5.QtCore as Core
 from easypie.user_bindings import on_key, screen, run
 from easypie.abstract import GameElement
 init()
+
+KMOD_ALT = Core.Qt.AltModifier
+KMOD_CTRL = Core.Qt.ControlModifier
+KMOD_META = Core.Qt.MetaModifier
+KMOD_NONE = Core.Qt.NoModifier
+KMOD_SHIFT = Core.Qt.ShiftModifier
+
+KEYDOWN = pygame.KEYDOWN
+KEYUP = pygame.KEYUP
+KEYPRESSED = 0
+
+WIDTH = 1600
+HEIGHT = 900
+SCREEN_SIZE = (WIDTH, HEIGHT)
