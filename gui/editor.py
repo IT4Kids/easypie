ba18f4e914ef73a3f83308d642385dab534b9e3a
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-09 10:03:57 +0200

    Cleaned up unnecessary imports.

--- gui/editor.py
+++ gui/editor.py
@@ -1,8 +1,4 @@
 # coding=utf-8
-from PyQt5 import QtGui
-from PyQt5.QtGui import QColor
-
-from pyqode.core.api import TextDecoration
 from pyqode.python.backend import server
 from pyqode.python.widgets import PyCodeEdit
 from pyqode.python.modes import PEP8CheckerMode
