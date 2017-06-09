b9310a1d9d3236c2c4924221e49f35726c2e5e4c
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-09 10:04:23 +0200

    Made pong-example work with new features.

--- games/pong.py
+++ games/pong.py
@@ -1,17 +1,17 @@
 # coding=utf-8
 import math
 import random
-import game
+import game as pygame
 
 
-class Ball(game.GameElement):
+class Ball(pygame.GameElement):
     # Constructor. Pass in the color of the block, and its x and y position
     def __init__(self):
         # Call the parent class (Sprite) constructor
         super().__init__()
         self.width, self.height = self.size = (20, 20)
         # Create the image of the ball
-        self.image = game.Surface(self.size)
+        self.image = pygame.Surface(self.size)
 
         # Color the ball
         self.image.fill((255, 255, 255))
@@ -35,8 +35,8 @@ class Ball(game.GameElement):
 
     def reset(self):
         self.speed = 6.0
-        self.x = game.WIDTH / 2
-        self.y = game.HEIGHT / 2
+        self.x = pygame.WIDTH / 2
+        self.y = pygame.HEIGHT / 2
         # Direction of ball (in degrees)
         if random.randint(0,1):
             self.direction = random.randrange(45,135)
@@ -57,7 +57,7 @@ class Ball(game.GameElement):
         self.x += self.speed * math.sin(direction_radians)
         self.y -= self.speed * math.cos(direction_radians)
 
-        if self.y <= 0 or self.y > game.HEIGHT - self.height:
+        if self.y <= 0 or self.y > pygame.HEIGHT - self.height:
             self.bounce(0)
 
         # Move the image to where our x and y are
@@ -65,49 +65,48 @@ class Ball(game.GameElement):
         self.rect.y = self.y
 
 
-class Player(game.GameElement):
+class Player(pygame.GameElement):
     # Constructor function
     def __init__(self, x_pos):
         # Call the parent's constructor
         super().__init__()
         self.width = 15
         self.height = 150
-        self.image = game.Surface([self.width, self.height])
+        self.image = pygame.Surface([self.width, self.height])
         self.image.fill((255, 255, 255))
 
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
 
         self.rect.x = x_pos
-        self.rect.y = game.HEIGHT / 2
+        self.rect.y = pygame.HEIGHT / 2
 
     # Update the player
     def move(self, distance):
         next_pos = self.rect.y + distance
-        if 0 < next_pos < game.HEIGHT-self.height:
+        if 0 < next_pos < pygame.HEIGHT-self.height:
             self.rect.y = next_pos
 
 
 p1_score = 0
 p2_score = 0
-screen = game.screen
-font = game.font.Font(None, 36)
+screen = pygame.screen
+font = pygame.font.Font(None, 36)
 ball = Ball()
 p1 = Player(50)
-p2 = Player(game.WIDTH - 65)
+p2 = Player(pygame.WIDTH - 65)
 
-balls = game.sprite.Group()
+balls = pygame.sprite.Group()
 balls.add(ball)
 
-moving_sprites = game.sprite.Group()
+moving_sprites = pygame.sprite.Group()
 moving_sprites.add(ball)
 moving_sprites.add(p1)
 moving_sprites.add(p2)
-
-game.on_key("a", lambda: p1.move(10), method=game.KEYPRESSED)
-game.on_key("d", lambda: p1.move(-10), method=game.KEYPRESSED)
-game.on_key("j", lambda: p2.move(10), method=game.KEYPRESSED)
-game.on_key("l", lambda: p2.move(-10), method=game.KEYPRESSED)
+pygame.on_key(pygame.K_a, lambda: p1.move(10), method=pygame.KEYPRESSED)
+pygame.on_key(pygame.K_d, lambda: p1.move(-10), method=pygame.KEYPRESSED)
+pygame.on_key(pygame.K_j, lambda: p2.move(10), method=pygame.KEYPRESSED)#todo support all keys, not only letters and numbers
+pygame.on_key(pygame.K_l, lambda: p2.move(-10), method=pygame.KEYPRESSED)
 
 
 def game_loop():
@@ -126,7 +125,7 @@ def game_loop():
     textpos = (1450, 0)
     screen.blit(text, textpos)
 
-    if game.sprite.spritecollide(p1, balls, False):
+    if pygame.sprite.spritecollide(p1, balls, False):
         # The 'diff' lets you try to bounce the ball left or right depending where on the paddle you hit it
         diff = 180 + random.randint(-10, 10)  # (p1.rect.x + p1.width / 2) - (ball.rect.x + ball.width / 2)
 
@@ -136,12 +135,12 @@ def game_loop():
         ball.bounce(diff)
 
         # See if the ball hits the player paddle
-    if game.sprite.spritecollide(p2, balls, False):
+    if pygame.sprite.spritecollide(p2, balls, False):
         # The 'diff' lets you try to bounce the ball left or right depending where on the paddle you hit it
         diff = 180 + random.randint(-10, 10)  # (p2.rect.x + p2.width / 2) - (ball.rect.x + ball.width / 2)
 
         # Set the ball's y position in case we hit the ball on the edge of the paddle
-        ball.x = game.WIDTH - 80
+        ball.x = pygame.WIDTH - 80
         ball.speed += 1
         ball.bounce(diff)
 
@@ -160,4 +159,4 @@ def game_loop():
 
     moving_sprites.draw(screen)
 
-game.run(game_loop)
+pygame.run(game_loop)
