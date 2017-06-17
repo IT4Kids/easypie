8a808bd9874f699ad72a22518fddd790789f40f2
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-17 12:35:48 +0200

    Temporarily put user-thread logic in main for redesign.

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 # coding=utf-8
-import pygame
+import threading
 
-import easypie.core.game_bindings as game
+import pygame
+import sys
+
+import commons
+import easypie.signals
+import easypie.core.interpreter
 import easypie.gui.gui_core as gui
 
-pygame.init()
-gui.init(game.screen)
-gui.main_window.centralWidget().editor.file.open('/home/axxessio/workspace/Work/IT4Kids/easypie/examples/MausZumKaese/vorlage.py')
-ret = gui.app.exec_()
-pygame.quit()
+interpreter_thread = None
+
+
+class InterpreterThread(threading.Thread):
+    def __init__(self, code):
+        super().__init__()
+        self.code_string = code
+        self.stop_flag = threading.Event()
+        self.setDaemon(True)
+
+    def stop(self):
+        self.stop_flag.set()
+
+    def run(self):
+        easypie.core.interpreter.interpret(self.code_string)
+
+def run_code(code):
+    global interpreter_thread
+    if interpreter_thread:
+        interpreter_thread.stop()#todo async raise?
+        interpreter_thread.join()
+    interpreter_thread = InterpreterThread(code)
+    interpreter_thread.start()
+
+easypie.signals.all.game_start_signal.connect(run_code)
+
+if __name__ == '__main__':
+    pygame.init()
+    gui.init(commons.screen)
+    ret = gui.app.exec_()
+    pygame.quit()
+    sys.exit(ret)
