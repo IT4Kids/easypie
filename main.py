2120c8035a8fe3063a26a2d820af33d5e4ab30b0
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-22 16:35:51 +0200

    Fixed another bug when with frozen version. Made sure that PyInstaller will add examples to files-list.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # coding=utf-8
-
 import easypie.gui.gui_core as gui
 import easypie.core.game_bindings as game
+import examples.MausZumKaese.maus_zum_kaese_main #TODO Restructure, temporary fix for missing files in dist version
 
 gui.init(game.screen)
 gui.main_window.centralWidget().editor.file.open('/home/axxessio/workspace/Work/IT4Kids/easypie/examples/MausZumKaese/vorlage.py')
