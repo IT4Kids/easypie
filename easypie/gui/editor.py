3e1e6a54836237670c39b69b4f7f5fe254afab1f
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-22 16:15:04 +0200

    Removed auto-start backend since this causes a very weird bug. The backend server/interpreter is started as a QProcess and relies on sys.executable for the Python interpreter. However, when frozen, sys.executable does not contain .../bin/python3 but the path to this very program, resulting in an endless cycle of the program spawning copies of itself. Not very desireable, hence I've removed CodeCompletion until this bug is fixed.

--- easypie/gui/editor.py
+++ easypie/gui/editor.py
@@ -1,11 +1,15 @@
 # coding=utf-8
 from pyqode.python.modes import PEP8CheckerMode
+from pyqode.python.panels import QuickDocPanel
+from pyqode.core.modes import CodeCompletionMode
 from pyqode.python.widgets import PyCodeEdit
 
 
 class Editor(PyCodeEdit):
     # https://pythonhosted.org/pyqode.python/examples.html
     def __init__(self):
-        super().__init__(color_scheme='monokai')
+        super().__init__(color_scheme='monokai', autostart_backend=False)
         self.modes.remove(PEP8CheckerMode)
+        self.modes.remove(CodeCompletionMode)
+        self.panels.remove(QuickDocPanel)
 
