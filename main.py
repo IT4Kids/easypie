4e9d870cb8483b5a729620aad321e644e59002e9
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-22 10:47:32 +0200

    Recommited correct import order.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
-import easypie.core.game_bindings as game
 import easypie.gui.gui_core as gui
+import easypie.core.game_bindings as game
 
 gui.init(game.screen)
 gui.main_window.centralWidget().editor.file.open('/home/axxessio/workspace/Work/IT4Kids/easypie/examples/MausZumKaese/vorlage.py')
