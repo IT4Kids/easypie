a5a95aa8084d6e15eb1671c3cc84c284db9ce8a4
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-22 10:39:00 +0200

    Removed unnecessary logic, that was there for testing reasons.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 # coding=utf-8
-import pygame
 
-import easypie.gui.gui_core as gui
 import easypie.core.game_bindings as game
+import easypie.gui.gui_core as gui
 
-pygame.init()
 gui.init(game.screen)
 gui.main_window.centralWidget().editor.file.open('/home/axxessio/workspace/Work/IT4Kids/easypie/examples/MausZumKaese/vorlage.py')
-ret = gui.app.exec_()
-pygame.quit()
+gui.app.exec()
