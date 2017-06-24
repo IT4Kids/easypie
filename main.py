8f866419b8883aeccdd830e08f62cea4f9038428
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-24 19:22:20 +0200

    Pep it up: added the mighty main check.

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import src.gui.gui_core as gui
 import src.core.game_bindings as game
 import examples.MausZumKaese.maus_zum_kaese_main #TODO Restructure, temporary fix for missing files in dist version
 
-gui.init(game.screen)
-gui.app.exec()
+if __name__ == '__main__':
+    gui.init(game.screen)
+    gui.app.exec()
