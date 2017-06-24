48ee6b415c1362de5b870295a6d2d75fcc4ecff9
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-24 19:21:50 +0200

    Added check for frozen env to start code backend when not frozen.

--- src/gui/editor.py
+++ src/gui/editor.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import sys
 from pyqode.python.modes import PEP8CheckerMode, CalltipsMode
 from pyqode.python.panels import QuickDocPanel
 from pyqode.core.modes import CodeCompletionMode
@@ -9,11 +10,14 @@ import src.signals
 class MyCodeEdit(PyCodeEdit):
     # https://pythonhosted.org/pyqode.python/examples.html
     def __init__(self, parent=None):
-        super().__init__(parent=parent,color_scheme='monokai', autostart_backend=False)
+
+
+        super().__init__(parent=parent,color_scheme='monokai', autostart_backend=not getattr(sys,'frozen',False))
         self.modes.remove(PEP8CheckerMode)
-        self.modes.remove(CodeCompletionMode)
-        self.modes.remove(CalltipsMode)
-        self.panels.remove(QuickDocPanel)
+        if not self.backend.running:
+            self.modes.remove(CodeCompletionMode)
+            self.modes.remove(CalltipsMode)
+            self.panels.remove(QuickDocPanel)
 
 
 class Editor(SplittableCodeEditTabWidget):
@@ -21,7 +25,6 @@ class Editor(SplittableCodeEditTabWidget):
     def __init__(self,parent=None):
         super().__init__(parent)
         self.register_code_edit(MyCodeEdit)
-        self.create_new_document(base_name="NewFile",extension=".py")
         src.signals.all.save_as_signal.connect(self.save_current_as)
         src.signals.all.save_signal.connect(self.save_current)
 
