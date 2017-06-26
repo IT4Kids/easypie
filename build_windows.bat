ab587e9d9c0ebb1e8fc565facb665295968f24a0
Fynn Mazurkiewicz <FynnMazurkiewicz@users.noreply.github.com>
2017-06-26 17:29:12 +0200

    Addded comments for build-complications.

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,7 @@
 @echo off
+REM This script requires a very recent version of PyInstaller due to changes in PyQt5 structure.
+REM See: https://github.com/pyinstaller/pyinstaller/pull/2403, applying it will fix build issues such as PyQt5.dll not found.
+REM All warnings that include the windows-crt may be safely ignored.
 pyinstaller --onefile --noconsole easypie_pyinstaller.spec --distpath dist\windows
 echo y | mkdir dist\windows\demo_projects 
 xcopy demo_projects dist\windows\demo_projects\ /s/h/e/k/f/c/y
