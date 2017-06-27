cb664abd4fc9d150020ff190b6fdd135f262edc0
Fynn Mazurkiewicz <fynnmazurkiewicz@gmail.com>
2017-06-27 11:37:03 +0200

    Updated Editor to make sure that the backend isn't started when frozen. (Still because of the bug that will reopen the entire app due to incorrect args in sys.executable)

--- src/gui/editor.py
+++ src/gui/editor.py
@@ -1,26 +1,94 @@
 # coding=utf-8
 import sys
 
-from pyqode.python.modes import PEP8CheckerMode, CalltipsMode
-from pyqode.python.panels import QuickDocPanel
-from pyqode.core.modes import CodeCompletionMode
+from pyqode.core import modes, panels, api
+from pyqode.core.api import ColorScheme
+from pyqode.python import modes as pymodes
+from pyqode.python import panels as pypanels
 from pyqode.core.widgets import SplittableCodeEditTabWidget
-from pyqode.python.widgets import PyCodeEdit
+from pyqode.python.backend import server, defined_names
+from pyqode.python.folding import PythonFoldDetector
+from pyqode.python.widgets import PyCodeEditBase
 
 import signals
 
-class MyCodeEdit(PyCodeEdit):
-    # https://pythonhosted.org/pyqode.python/examples.html
-    def __init__(self, parent=None):
+class MyCodeEdit(PyCodeEditBase):
+    """
+    Extends PyCodeEditBase with a set of hardcoded modes and panels specifics
+    to a python code editor widget.
+    """
+    DARK_STYLE = 0
+    LIGHT_STYLE = 1
 
+    mimetypes = ['text/x-python']
 
-        super().__init__(parent=parent,color_scheme='monokai')
-        self.modes.remove(PEP8CheckerMode)
-        if not getattr(sys,'frozen',False):
-            self.backend.stop()
-            self.modes.remove(CodeCompletionMode)
-            self.modes.remove(CalltipsMode)
-            self.panels.remove(QuickDocPanel)
+    def __init__(self, parent=None, server_script=server.__file__,
+                 interpreter=sys.executable, args=None,
+                 create_default_actions=True, color_scheme='monokai',
+                 reuse_backend=False):
+        super().__init__(
+            parent=parent, create_default_actions=create_default_actions)
+
+        if not getattr(sys, 'frozen', False):
+            self.backend.start(server_script, interpreter, args,
+                           reuse=reuse_backend)
+
+        self.setLineWrapMode(self.NoWrap)
+        self.setWindowTitle("pyQode - Python Editor")
+
+        # install those modes first as they are required by other modes/panels
+        self.modes.append(modes.OutlineMode(defined_names))
+
+        # panels
+        self.panels.append(panels.SearchAndReplacePanel(),
+                           panels.SearchAndReplacePanel.Position.BOTTOM)
+        self.panels.append(panels.FoldingPanel())
+        self.panels.append(panels.LineNumberPanel())
+        self.panels.append(panels.CheckerPanel())
+        self.panels.append(panels.GlobalCheckerPanel(),
+                           panels.GlobalCheckerPanel.Position.RIGHT)
+        self.add_separator()
+
+        # modes
+        # generic
+        self.modes.append(modes.ExtendedSelectionMode())
+        self.modes.append(modes.CaseConverterMode())
+        self.modes.append(modes.CaretLineHighlighterMode())
+        self.modes.append(modes.FileWatcherMode())
+        self.modes.append(modes.RightMarginMode())
+        self.modes.append(modes.ZoomMode())
+        self.modes.append(modes.SymbolMatcherMode())
+        self.modes.append(modes.CodeCompletionMode())
+        self.modes.append(modes.OccurrencesHighlighterMode())
+        self.modes.append(modes.SmartBackSpaceMode())
+        # python specifics
+        if not getattr(sys, 'frozen', False):
+            self.modes.append(pymodes.CalltipsMode())
+            self.modes.append(pymodes.PyAutoCompleteMode())
+            self.panels.append(pypanels.QuickDocPanel(), api.Panel.Position.BOTTOM)
+
+        self.modes.append(pymodes.PyAutoIndentMode())
+        self.modes.append(pymodes.PyFlakesChecker())
+        self.modes.append(pymodes.PEP8CheckerMode())
+        self.modes.append(pymodes.PyIndenterMode())
+        self.modes.append(pymodes.GoToAssignmentsMode())
+        self.modes.append(pymodes.CommentsMode())
+        self.modes.append(pymodes.PythonSH(
+            self.document(), color_scheme=ColorScheme(color_scheme)))
+        self.syntax_highlighter.fold_detector = PythonFoldDetector()
+        self.panels.append(panels.EncodingPanel(), api.Panel.Position.TOP)
+        self.panels.append(panels.ReadOnlyPanel(), api.Panel.Position.TOP)
+
+    def clone(self):
+        clone = self.__class__(
+            parent=self.parent(), server_script=self.backend.server_script,
+            interpreter=self.backend.interpreter, args=self.backend.args,
+            color_scheme=self.syntax_highlighter.color_scheme.name)
+        return clone
+
+    def __repr__(self):
+        return 'PyCodeEdit(path=%r)' % self.file.path
+
 
 
 class Editor(SplittableCodeEditTabWidget):
